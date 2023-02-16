438410c751331e425f33427f6f206dd4f7767052
adrian <gogosadrian@outlook.com>
2023-02-16 22:56:39 +0100

    open ai

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot,has_permissions
 import dotenv
 import os
+from  src.open_ai import get_open_ai
 
 dotenv.load_dotenv()
 
@@ -29,6 +30,13 @@ async def szia(ctx):
 async def ban( ctx, member: discord.Member, *, reason=None):
      await member.ban(reason=reason)
      await ctx.send(f'User {member} has been kick')
+
+@client.command()
+async def open_ai(ctx, message: str) -> None: 
+    
+    await ctx.send("Nagy gondolkozasokba....")
+    await ctx.send(get_open_ai(message))
+    
 
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
 
---
+++ src/open_ai.py
@@ -0,0 +1,29 @@
+import openai
+import dotenv
+import os 
+
+dotenv.load_dotenv("../.env")
+
+# Replace YOUR_API_KEY with your OpenAI API key
+openai.api_key = os.environ.get("OPEN_AI_TOKEN")
+
+def get_open_ai(prompt: str) -> str:
+
+    model_engine = "text-davinci-003"
+
+    # Set the maximum number of tokens to generate in the response
+    max_tokens = 1024
+
+    # Generate a response
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=0.5,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Print the response
+    return completion.choices[0].text
