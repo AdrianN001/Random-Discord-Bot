4abc430a6b904af67cae4e6cd09abef6bcf5dd7f
adrian <gogosadrian@outlook.com>
2023-02-16 21:49:44 +0100

    asd

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import discord
 import dotenv
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.command()
+async def szia(ctx): 
+    await ctx.send("asdasd")
+
+
+client.run('your token here')
+
