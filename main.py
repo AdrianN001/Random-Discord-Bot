7982fad0cde4c7fcd381f99a32c0910ef80537c5
adrian <gogosadrian@outlook.com>
2023-02-16 22:01:22 +0100

    bannszolas

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import discord
+from discord.ext import commands
 import dotenv
 
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+description = "ASDSD"
+
+client = discord.Client(command_prefix='@',description=description, intents=intents)
 
 @client.event
 async def on_ready():
@@ -12,5 +20,12 @@ async def szia(ctx):
     await ctx.send("asdasd")
 
 
-client.run('your token here')
+# The below code bans player.
+@client.command()
+@client.has_permissions(ban_members=True)
+async def ban(self, ctx, member: discord.Member, *, reason=None):
+     await member.ban(reason=reason)
+     await ctx.send(f'User {member} has been kick')
+
+client.run(os.environ.get("DISCORD_BOT_TOKEN"))
 
