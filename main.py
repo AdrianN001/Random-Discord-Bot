d8f5dd3737f7f9f5d3cb99835ca9ac9e6c6fde93
adrian <gogosadrian@outlook.com>
2023-02-16 22:23:15 +0100

    working

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot,has_permissions
 import dotenv
+import os
+
+dotenv.load_dotenv()
 
 
-intents = discord.Intents.default()
+intents = discord.Intents.default().all()
 intents.members = True
-intents.message_content = True
 
 description = "ASDSD"
 
-client = discord.Client(command_prefix='@',description=description, intents=intents)
+client = Bot(command_prefix='@',description=description, intents=intents)
 
 # @client.event
 # async def on_ready():
@@ -22,7 +25,7 @@ async def szia(ctx):
 
 # The below code bans player.
 @client.command()
-@client.has_permissions(ban_members=True)
+@has_permissions(ban_members=True)
 async def ban(self, ctx, member: discord.Member, *, reason=None):
      await member.ban(reason=reason)
      await ctx.send(f'User {member} has been kick')
